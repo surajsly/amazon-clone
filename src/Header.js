9a21354b87aa51cc751b9b245e7e5fb23b6131d3
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-20 23:18:32 +0530

    homepage setup

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header__search">
+        <input className="header_searchInput" type="text" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header_optionLineOne">Return</span>
+          <span className="header_optionLineTwo">& Order</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKez0Jv3Yr1vK417AwAb9VYRRmz0e1lC0",
+  authDomain: "challenge-a1e3a.firebaseapp.com",
+  databaseURL: "https://challenge-a1e3a.firebaseio.com",
+  projectId: "challenge-a1e3a",
+  storageBucket: "challenge-a1e3a.appspot.com",
+  messagingSenderId: "205150496216",
+  appId: "1:205150496216:web:1ac27874065c2ba95b3ead",
+  measurementId: "G-BNG5H87B6Y",
+};

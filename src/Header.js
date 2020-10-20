8573c9a859ac9f36c37912b1ee1323cb49b8a96e
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-21 00:47:03 +0530

    search input

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 export const Header = () => {
   return (
@@ -11,6 +12,7 @@ export const Header = () => {
 
       <div className="header__search">
         <input className="header_searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">

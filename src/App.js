4ab6bc9056fa80c50e92fa747c4f8a86b0901d57
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-23 19:53:40 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      {/**/}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

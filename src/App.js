15d565e9405968f9bcf4535ee3a253ce1b558126
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-24 19:21:38 +0530

    subtotal component done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Checkout from "./Checkout.js";
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>

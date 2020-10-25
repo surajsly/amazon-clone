61e728a4829044fefa114a79285bebf7962feede
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-25 18:53:47 +0530

    adding login page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import Login from "./Login.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout.js";
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        className="login__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+      />
+    </div>
+  );
+}
+export default Login;

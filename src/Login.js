1cad1569d94ec53bfdc558090a50a864030b7501
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-25 19:18:37 +0530

    login front end done

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,47 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
   return (
     <div className="login">
-      <img
-        className="login__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-      />
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>Email</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+          />
+
+          <button className="login__signInButton">Sign in</button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button className="login__registerButton">Create amazon account</button>
+      </div>
     </div>
   );
 }
